ee10df1e76b32e209bcba0cb8566ae6e33a6d620
hkjj293 <47801987+hkjj293@users.noreply.github.com>
2021-11-30 14:14:49 +0000

    T2 complete

--- CSC8503/CSC8503Common/PhysicsSystem.cpp
+++ CSC8503/CSC8503Common/PhysicsSystem.cpp
@@ -262,7 +262,7 @@ void PhysicsSystem::IntegrateVelocity(float dt) {
 	std::vector<GameObject*>::const_iterator first;
 	std::vector<GameObject*>::const_iterator last;
 	gameWorld.GetObjectIterators(first, last);
-	float frameLinearDamping = 1.0f - (0.4f * dt);
+	float frameLinearDamping = 1.0f - (0.9f * dt);
 
 	for (auto i = first; i != last; ++i) {
 		PhysicsObject* object = (*i)->GetPhysicsObject();
